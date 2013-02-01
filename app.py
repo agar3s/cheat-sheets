d8e758182dd2f75d2c74fe95e72b3399d20196a1
Agares <ekeisco@gmail.com>
2013-02-01 02:02:06 -0500

    basic templating

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask.ext.pymongo import PyMongo
+from flask import render_template
+
 app = Flask(__name__)
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-   return 'Create your own Cheat Sheet online!'
+   return render_template('index.html')
+
+app.debug = True
 
 if __name__ == '__main__':
    app.run()
